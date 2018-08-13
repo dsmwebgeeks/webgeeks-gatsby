94d1c06fc4dd2f755d4d84c05e5fe2874066bcca
Josh Larson <jplhomer@gmail.com>
2018-08-13 15:50:06 -0500

    Continue the doldrum of writing inline styles

--- src/components/event.js
+++ src/components/event.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default ({ event }) => (
   <div>
     <img src={event.featured_media.source_url} alt={event.title} />
-    <h3>{event.title}</h3>
+    <h3
+      style={{ fontSize: '1.2em' }}>
+      {event.title}
+    </h3>
     <p>{event.acf.event_date}</p>
   </div>
 )
